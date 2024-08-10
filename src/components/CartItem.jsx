b3e67b5ceb3ca25ac56d2c396297bc2e8d2bc52a
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-08-10 10:00:21 +0530

    Updating codes

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,33 @@
-const CartItem = () => {
+import { FcDeleteDatabase } from "react-icons/fc";
+import { useDispatch } from "react-redux";
+import {remove} from "../redux/Slices/CartSlice";
+import {toast} from "react-toastify"
+
+const CartItem = ({item, itemIndex}) => {
+    const dispatch = useDispatch();
+
+    const removeFromCart = () => {
+        dispatch(remove(item.id));
+        toast.success("Item removed successfully");
+    }
     return(
-        <div>CartItem</div>
+        <div>
+            <div>
+                <img src={item.image} alt="" />
+            </div> 
+            <div>
+                <h1>{item.title}</h1>
+                <p>{item.description}</p>
+                <div>
+                    <p>{item.price}</p>
+                    <div
+                        onClick={removeFromCart} 
+                    >
+                        <FcDeleteDatabase/>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 };
 
