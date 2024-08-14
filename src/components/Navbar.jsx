e6168273dd3e133d5a8e6f79f6424fa555d903ef
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-08-14 10:07:20 +0530

    Updated tailwind css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,38 @@
 import { FaShoppingCart } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 const Navbar = () => {
+    const {cart} = useSelector((state) => state);
     return(
-        <div className="bg-gray-300">
-            <div className="flex flex-row justify-between ">
+        <div>
+            <nav className="flex justify-between items-center h-20 max-w-6xl mx-auto ">
                 <NavLink to="/" >
-                    <div>
+                    <div className="ml-5">
                         <img alt="" src="logo.png" width={100} />
                     </div>
                 </NavLink>
 
-                <NavLink to="/">
-                    <p>Home</p>
-                </NavLink>
+                <div className="flex items-center font-medium text-slate-100 mr-5 space-x-6 ">
+                    <NavLink to="/">
+                        <p>Home</p>
+                    </NavLink>
 
-                <NavLink to="/cart">
-                    <FaShoppingCart/>
-                </NavLink>
+                    <NavLink to="/cart">
+                        <div className="relative">
+                            <FaShoppingCart className="text-2xl" />
+                            {
+                                cart.length > 0 && (
+                                <span className="absolute -top-1 -right-2 bg-green-600 text-xs w-5 h-5 
+                                flex justify-center items-center rounded-full text-white animate-bounce ">{cart.length}</span>
+                                )
+                            }
+                        </div>
+                    </NavLink>
+                </div>
 
 
-            </div>
+            </nav>
 
             
         </div>
