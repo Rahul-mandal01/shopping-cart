f59aed6a4b65fdf7e89012b1f72646bd289ae688
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-08-15 20:38:32 +0530

    Updated CSS

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -21,22 +21,21 @@ const Product = ({post}) =>{
     return (
         <div className="group flex flex-col items-center justify-between hover:scale-110 transition 
                 duration-300 ease-in gap-3 p-4 mt-10 ml-5 rounded-xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]
-                 hover:shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]
-                 " >
+                 hover:shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]" >
             <div>
-                <p className="text-gray-700 font-semibold text-lg text-left truncate w-48 mt-1" >{post.title}</p>
+                <p className="text-gray-700 font-semibold text-lg text-left truncate w-40 mt-1" >{post.title}</p>
             </div>
-            <div>
-                <p className="w-40 text-gray-400 font-normal text-[10px] text-left " >{post.description.split(" ").slice(0,10).join(" ") + "..." }</p>
+            <div >
+                <p className="w-40 text-gray-400 font-normal text-[10px] text-left leading " >{post.description.split(" ").slice(0,10).join(" ") + "..." }</p>
             </div>
             <div className="h-[180px]">
-                <img src={post.image} alt="" className="h-full w-full" />
+                <img src={post.image} alt="" className="h-full w-full object-contain " />
             </div>
             <div>
                 <img src={post.price} alt=""/>
             </div>
 
-            <div className="flex justify-between gap-12 items-center w-full mt-5 ">
+            <div className="flex justify-between items-center w-full mt-2 ">
 
                 <div >
                     <p className="text-green-600 font-semibold " >${post.price}</p>
