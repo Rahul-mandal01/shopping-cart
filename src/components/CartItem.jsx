feef7256bed9baa6d133ad5323827993f1b40488
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-08-15 20:38:15 +0530

    Updated CSS

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -11,17 +11,18 @@ const CartItem = ({item, itemIndex}) => {
         toast.success("Item removed successfully");
     }
     return(
-        <div>
-            <div>
-                <img src={item.image} alt="" />
+        <div className="flex items-center p-2 md:p-5 justify-between border-b-2 border-slate-500  mt-2 mb-2 md:mx-5" >
+            <div className="w-[30%]" >
+                <img src={item.image} alt="" className=" object-cover" />
             </div> 
-            <div>
-                <h1>{item.title}</h1>
-                <p>{item.description}</p>
-                <div>
-                    <p>{item.price}</p>
+            <div className="md:ml-10 self-start space-y-5 w-[100%] md:w-[70%]">
+                <h1 className="text-xl text-slate-700 font-semibold">{item.title}</h1>
+                <h1 className="text-base text-slate-700 font-medium " >{item.description.split(" ").slice(0,15).join(" ") + "..." } </h1>
+                <div className="flex items-center justify-between">
+                    <p className="font-bold text-lg text-green-600" >${item.price}</p>
                     <div
                         onClick={removeFromCart} 
+                        className=" bg-red-200 group hover:bg-red-400 transition-transform duration-300 cursor-pointer rounded-full p-3 mr-3"
                     >
                         <FcDeleteDatabase/>
                     </div>
